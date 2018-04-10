5126e314d36e383196b6ed9dad2c60893587e93e
zytfo <a.khayaliev@innopolis.ru>
2018-04-10 16:13:58 +0300

    Added constants

--- src/graph_db/fs/graph_storage.py
+++ src/graph_db/fs/graph_storage.py
@@ -1,9 +1,6 @@
 from graph_db.engine.types import *
 from .record_storage import RecordStorage
 
-NODE_RECORD_SIZE = 13
-RELATIONSHIP_RECORD_SIZE = 34
-
 
 class NodeStorage(RecordStorage):
     """
@@ -16,8 +13,35 @@ class NodeStorage(RecordStorage):
 
 class RelationshipStorage(RecordStorage):
     """
-    Physical Node Storage.
+    Physical Relationship Storage.
     """
 
     def __init__(self, path: str = RELATIONSHIP_STORAGE):
         super().__init__(RELATIONSHIP_RECORD_SIZE, path)
+
+
+class PropertyStorage(RecordStorage):
+    """
+    Physical Property Storage.
+    """
+
+    def __init__(self, path: str = PROPERTY_STORAGE):
+        super().__init__(PROPERTY_RECORD_SIZE, path)
+
+
+class LabelStorage(RecordStorage):
+    """
+    Physical Label Storage.
+    """
+
+    def __init__(self, path: str = LABEL_STORAGE):
+        super().__init__(LABEL_RECORD_SIZE, path)
+
+
+class DynamicStorage(RecordStorage):
+    """
+    Physical Dynamic Storage.
+    """
+
+    def __init__(self, path: str = DYNAMIC_STORAGE):
+        super().__init__(DYNAMIC_RECORD_SIZE, path)
