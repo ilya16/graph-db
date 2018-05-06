17ba8abdcd886fe5eb2d8a883bad543f74de7995
ilya16 <ilyaborovik97@gmail.com>
2018-05-06 23:02:27 +0300

    Console mode improvements
    + some installation fixes

--- src/graph_db/console/console.py
+++ src/graph_db/console/console.py
@@ -1,27 +1,28 @@
 from graph_db.access import db
-from graph_db.access.parser import InputError
 import sys
 import readline
 
-HELP = "Query examples:\n" \
-           "create graph: label\n" \
-           "create node: label\n" \
-           "create node: label key:value\n" \
-           "create node: label key:value key:value key:value\n" \
-           "create relationship: label from label1 to label2\n" \
-           "create relationship: label from label1 to label2 key:value\n" \
-           "create relationship: label from id:0 to id:1 key:value\n" \
-           "match node: label\n" \
-           "match node: id:0\n" \
-           "match relationship: label\n" \
-           "match relationship: id:1\n" \
-           "match node: key=value\n" \
-           "match node: key<value\n" \
-           "match relationship: key>=value\n" \
-           "match graph: label\n" \
-           "delete node: id:0\n" \
-           "delete relationship: id:0\n" \
-           "'exit' to close\n"
+HELP = """
+Query examples:
+create graph: label
+create node: label
+create node: label key:value
+create node: label key:value key:value key:value
+create relationship: label from label1 to label2
+create relationship: label from label1 to label2 key:value
+create relationship: label from id:0 to id:1 key:value
+match node: label
+match node: id:0
+match relationship: label
+match relationship: id:1
+match node: key=value
+match node: key<value
+match relationship: key>=value
+match graph: label
+delete node: id:0
+delete relationship: id:0
+`/exit` to close
+"""
 
 
 class ConsoleReader:
@@ -32,14 +33,15 @@ class ConsoleReader:
 
     def read_query(self):
         print("Welcome to Graph DB. (c) Ilya Borovik, Artur Khayaliev, Boris Makaev\n\n"
-              "You can enter '/help' to see query examples.\n")
+              "You can enter `/help` to see query examples.\n")
         while True:
             user_input = input("\n")
             if len(user_input) != 0:
                 if '/help' in user_input:
                     print(HELP)
                     continue
-                if user_input == 'exit':
+                if user_input == '/exit':
+                    self.db.close()
                     break
                 if 'create graph' not in user_input and self.db.get_graph() is None:
                     print('You have to create a graph firstly using \'create graph: label\'')
@@ -48,19 +50,20 @@ class ConsoleReader:
                     if not self.db.get_graph():
                         try:
                             self.cursor.execute(user_input)
-                        except InputError as e:
+                            result = self.cursor.fetch_one()
+                            print(f"You have created graph called '{result.get_name()}' ")
+                        except SyntaxError as e:
                             print(e)
                     else:
                         if 'create graph' in user_input:
-                            print(f"You have already created a graph called "
-                                  f"'{self.db.get_graph().get_name()}'")
+                            print(f"You have already created a graph called '{self.db.get_graph().get_name()}'")
                             continue
                         try:
                             self.cursor.execute(user_input)
                             result = self.cursor.fetch_all()
                             for r in result:
                                 print(r)
-                        except InputError as e:
+                        except SyntaxError as e:
                             print(e)
 
 
@@ -68,6 +71,9 @@ def run():
     base_dir = 'temp_db/'
     if len(sys.argv) > 1:
         base_dir = sys.argv[1]
-    print(base_dir)
-    reader = ConsoleReader()
+    reader = ConsoleReader(base_dir)
     reader.read_query()
+
+
+if __name__ == '__main__':
+    run()
